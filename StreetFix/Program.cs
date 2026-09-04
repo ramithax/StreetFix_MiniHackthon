086b290c112517e36b0137ca134c8ad97f2b1c67
IT24101463 <IT24101463@my.sliit.lk>
2026-09-04 11:20:11 +0530

    Backend V2.0 -Swagger Update

--- StreetFix/Program.cs
+++ StreetFix/Program.cs
@@ -19,6 +19,12 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
 
 app.MapControllers();
